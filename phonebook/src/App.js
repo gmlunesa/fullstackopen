6d3c1bd95b9a03a934496af48ef510b19366e2f8
Lunesa <gmlunesa@gmail.com>
2020-11-20 05:41:07 +0800

    Modified Phonebook for Part 3

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import phoneBookService from "./services/phoneBookService";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -14,7 +14,7 @@ const App = () => {
 
   // Define person-fetch hook
   const personHook = () => {
-    axios.get("http://localhost:3001/persons").then(({ data }) => {
+    phoneBookService.getAll().then((data) => {
       setPersons(data);
     });
   };
@@ -30,7 +30,29 @@ const App = () => {
 
     // If `name` already exists, send alert
     if (personIndex > -1) {
-      alert(`${newName} is already added to the phonebook`);
+      if (
+        window.confirm(
+          `${newName} is already added to the phonebook, replace the old number with a new one?`
+        )
+      ) {
+        // Create a new object based on the found person on the `persons` array
+        const nameObject = {
+          ...persons[personIndex],
+          number: newNumber,
+        };
+
+        // Update person on the server
+        // Then, update persons array on the local state
+        phoneBookService
+          .update(persons[personIndex].id, nameObject)
+          .then((response) => {
+            setPersons(
+              persons.map((person) =>
+                person.name === response.name ? response : person
+              )
+            );
+          });
+      }
     } else {
       // Create new object with the `name` and `number`
       const nameObject = {
@@ -38,12 +60,15 @@ const App = () => {
         number: newNumber,
       };
 
-      // Add new object to the persons array
-      setPersons(persons.concat(nameObject));
+      // Add new person object to the server
+      phoneBookService.add(nameObject).then(() => {
+        // Add new object to the persons array
+        setPersons(persons.concat(nameObject));
 
-      // Reset values in the input boxes
-      setNewName("");
-      setNewNumber("");
+        // Reset values in the input boxes
+        setNewName("");
+        setNewNumber("");
+      });
     }
   }
 
@@ -72,12 +97,25 @@ const App = () => {
 
   // Check the value of showAll
   // If true, assign contents of person
-  // If false, filter the values tht include the newSearch
+  // If false, filter the values that include the newSearch
   const personsToShow = showAll
     ? persons
     : persons.filter((person) =>
         person.name.toLowerCase().includes(newSearch.toLowerCase())
       );
+
+  // This function is called when user clicks the delete button
+  const handleDeleteClick = (personToDelete) => {
+    if (window.confirm(`Delete ${personToDelete.name}?`)) {
+      // Delete the person from the server
+      phoneBookService.deletePerson(personToDelete.id);
+
+      // Delete the person from our local state
+      setPersons(
+        persons.filter((person) => person.name !== personToDelete.name)
+      );
+    }
+  };
 
   return (
     <div>
@@ -92,7 +130,7 @@ const App = () => {
         numberOnChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      <Persons persons={personsToShow} />
+      <Persons persons={personsToShow} onClick={handleDeleteClick} />
     </div>
   );
 };
--- phonebook/src/components/Persons.js
+++ phonebook/src/components/Persons.js
@@ -1,7 +1,8 @@
-const Persons = ({ persons }) => {
+const Persons = ({ persons, onClick }) => {
   return persons.map((person) => (
     <p key={person.name}>
-      {person.name} : {person.number}
+      {person.name} : {person.number}&nbsp;
+      <button onClick={() => onClick(person)}>Delete</button>
     </p>
   ));
 };
